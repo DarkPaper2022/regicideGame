e2af92b4e7907bd93758b28b10507e03591847e2
DarkPaper <darkpaper2024@gmail.com>
2024-03-22 00:17:29 +0800

    delete old

--- defineRound.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class ROUND(Enum):
-    preparing = 0
-    atk = 1
-    defend = 2
-    jokerTime = 3
-    over = 4
--- myLogger.py
+++
@@ -1,6 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO,  # 设置日志级别为 INFO
-                    format='%(asctime)s - %(levelname)s - %(message)s',  # 设置日志格式
-                    filename='example.log',  # 指定日志文件名
-                    filemode='a')  # 设置日志文件模式为追加模式
-logger = logging.getLogger()
