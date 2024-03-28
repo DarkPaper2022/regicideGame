c8c64ed729475a5ba8c28c2c74717017b33f4a41
DarkPaper <darkpaper2024@gmail.com>
2024-03-28 16:02:36 +0800

    little

--- message_define/myLogger.py
+++ message_define/myLogger.py
@@ -3,7 +3,7 @@ from logging.handlers import TimedRotatingFileHandler
 
 # 创建日志记录器
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # 创建 TimedRotatingFileHandler，并配置日志文件格式和日期格式
 handler = TimedRotatingFileHandler(filename='logs/log', when='midnight', interval=1, backupCount=7)
